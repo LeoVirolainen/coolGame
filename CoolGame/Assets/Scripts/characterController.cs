4af89a9f69717443630fe75aecb03b5752721aa9
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2019-02-07 15:36:04 +0200

    Foo

---
+++ CoolGame/Assets/Scripts/characterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    public float speed = 5.0F;
+    public float jumpSpeed = 5f;
+    public Rigidbody rb; //for za jumping
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>(); //for za jumping
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rb.transform.position.y >= 1 && rb.transform.position.y <= 2)
+            rb.AddForce(Vector3.up * jumpSpeed); 
+        }
+    }
+    
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+}
---
+++ CoolGame/Assets/Scripts/gunScript.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+public class gunScript : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash; //pyssyn muzzle flash
+    public GameObject impactEffect; //osuman part. effect
+
+    private float nextTimeToFire = 0f; //for full-auto
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire) //poista "getButtonDown":ista "Down" -nii ampuu full-auto
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce); //täräyttää targetia
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal)); //liittyy impactin particleseihin
+            Destroy(impactGO, 2f); //tuhoo "impact" -assetit 2 sek. päästä
+        }
+    }
+}
