5d84d8a6d5919049d4cacc434e10fdf8ef3f9b0c
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2019-02-25 16:59:29 +0200

    moving overhaul + bobbing tests

---
+++ CoolGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public string horizontalInputName;
+    public string verticalInputName;
+    public float movementSpeed;
+
+    private CharacterController charController;
+
+    public Animation anim;
+
+    public AnimationCurve jumpFalloff;
+    public float jumpMultiplier;
+    public KeyCode jumpKey;
+    private bool isMoving;
+
+    private bool isJumping;
+
+    private void Start() {
+        anim = gameObject.GetComponentInChildren<Animation>();
+    }
+
+    private void Awake() {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        PlayerMovement();
+        if (gameObject.GetComponent<CharacterController>().velocity.z > 0) {
+            isMoving = true;
+        } else {
+            isMoving = false;
+            print("not moving");
+        }
+
+        if (gameObject.GetComponent<CharacterController>().velocity.x > 0) {
+            isMoving = true;
+        } else {
+            isMoving = false;
+            print("not moving in x");
+        }
+
+        if (isMoving == true) {
+            //anim.Play("Bobbing");
+            print("I'm moving. Should bob.");
+        }
+    }
+
+    private void PlayerMovement() {
+
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput();
+    }
+
+    private void JumpInput() {
+        if (Input.GetKeyDown(jumpKey) && !isJumping) {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent() {
+        charController.slopeLimit = 90.0f; //tämä estää glitchauksen seinää vasten hypätessä
+        float timeInAir = 0.0f;
+
+        do {
+            float jumpForce = jumpFalloff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
+
